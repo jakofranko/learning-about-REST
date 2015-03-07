4c7c85e0202f58e34c71a305dbac309f68a08641
Jake Franklin <jfranklin@telesphere.com>
2015-03-06 18:10:49 -0700

    added controllers, and models

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-// Get the packages we need
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var beerController = require('./controllers/beer');
+
+// Connect to the beerlocker MongoDB
+mongoose.connect('mongodb://localhost:27017/beerlocker');
 
 // Create our Express application
 var app = express();
 
-// Use environment defined port or 3000
-var port = process.env.PORT || 3000;
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on beer!' });
-});
+// Create endpoint handlers for /beers
+router.route('/beers')
+  .post(beerController.postBeers)
+  .get(beerController.getBeers);
+
+// Create endpoint handlers for /beers/:beer_id
+router.route('/beers/:beer_id')
+  .get(beerController.getBeer)
+  .put(beerController.putBeer)
+  .delete(beerController.deleteBeer);
 
 // Register all our routes with /api
 app.use('/api', router);
 
 // Start the server
-app.listen(port);
-console.log('Insert beer on port ' + port);
+app.listen(3000);
